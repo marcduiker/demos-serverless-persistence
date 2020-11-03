1f4ee10b7c57c545a96ae32b36f3ba7c072f6c35
Marc Duiker <marcduiker@gmail.com>
2020-11-03 22:35:11 +0100

    Added GetBlobNamesWithBlobContainerInput

---
+++ src/ServerlessPersistence/Blob/Input/GetBlobNamesWithBlobContainerInput.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace ServerlessPersistence.Blob.Output
+{
+    public static class GetBlobNamesWithBlobContainerInput
+    {
+        [FunctionName(nameof(GetBlobNamesWithBlobContainerInput))]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request,
+            [Blob("players", FileAccess.Read)] CloudBlobContainer cloudBlobContainer
+        )
+        {
+            var blobList = cloudBlobContainer.ListBlobs(
+                prefix: "in/", 
+                useFlatBlobListing: true, 
+                blobListingDetails: BlobListingDetails.Metadata).OfType<CloudBlockBlob>();
+            var blobNames = blobList.Select(blob => new { BlobName = blob.Name });
+
+            return new OkObjectResult(blobNames);
+        }
+    }
+}
